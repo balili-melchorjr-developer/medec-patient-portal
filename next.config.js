3df184c0ffe149605bd6317a205348535769a305
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-11-25 17:08:25 +0800

    add digitalocean spaces config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/cdn/:path*', // Define the path to be proxied
+          destination: 'https://medec-bucket.sgp1.digitaloceanspaces.com/:path*', // Replace with your DigitalOcean Spaces endpoint
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
